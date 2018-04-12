020f0c963ef60891dee18fa8978c024ffd9d41ec
bekasovme <bekasovme@gmail.com>
2018-04-12 13:17:56 +0300

    v.2.1

--- animal.php
+++ animal.php
@@ -3,7 +3,7 @@ $countinents = array(
     'africa' => array('macaca sylvanus', 'panthera pardus', 'felis margarita'),
     'eurasia' => array('capra', 'meleseles meles', 'tarsius'),
     'north america' => array('rangifer tarandus', 'lynx rufus', 'canis latrans'),
-    'south america' => array('vicugna vicugna', 'hydrochoerus hydrochaeris', 'panthera onca'),
+    'south america' => array('vicugna vicugna', 'bubu bubu bubu', 'hydrochoerus hydrochaeris', 'panthera onca'),
     'australia' => array('canis lupus dingo',	'macropodidae', 'raphus cucullatus'),
     'antarctica' => array('ursus maritimus', 'aptenodytes patagonicus', 'nyctea scandiaca')
 );
