6746fb5718536b6070d35a28eae3a0f70729d77c
bekasovme <bekasovme@gmail.com>
2018-04-12 14:35:41 +0300

    v.2.5

--- animal.php
+++ animal.php
@@ -22,7 +22,7 @@ foreach ($countinents as $country => $animals) {
 echo '<pre>';
 var_dump($new_array);
 
-echo
+echo count(explode(' ', $new_array));
 //
 //$animal1 = $animal2 = [];
 //foreach($new_array as $value)
